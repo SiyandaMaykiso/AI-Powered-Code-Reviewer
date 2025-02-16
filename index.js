24350cb5a6a5397589840bd666b2cceb68bf800d
Siyanda Mayekiso <s.mayekiso@gmail.com>
2025-02-16 10:42:34 -0600

    Fix CORS and vite config for Heroku

--- index.js
+++ index.js
@@ -9,20 +9,8 @@ dotenv.config();
 const app = express();
 
 // ✅ CORS Setup
-const allowedOrigins = [
-    'http://localhost:5173',
-    'http://localhost:3000',
-    process.env.FRONTEND_URL
-];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: '*',
     credentials: true,
     optionsSuccessStatus: 200
 }));
